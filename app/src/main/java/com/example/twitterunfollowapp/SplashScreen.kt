a4bb99ce5c2bf63bc6b3a766cd2e88d774a0bac5
Adedoyin <isradeoluwa@gmail.com>
2021-01-22 16:12:34 +0100

    Splash Screen: Splash Screen development completed

--- app/src/main/java/com/example/twitterunfollowapp/SplashScreen.kt
+++ app/src/main/java/com/example/twitterunfollowapp/SplashScreen.kt
@@ -1,7 +1,9 @@
 package com.example.twitterunfollowapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
@@ -11,6 +13,11 @@ class SplashScreen : AppCompatActivity() {
 
         app_logo_splash_screen.animate().alpha(1.0F).setDuration(5000)
         app_name_splash_screen.animate().translationY(-100F).setDuration(5000)
+
+        Handler().postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }, 7000)
 
     }
 }
