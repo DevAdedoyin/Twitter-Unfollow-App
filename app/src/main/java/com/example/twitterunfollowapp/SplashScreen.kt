bcc6ae82189128bb7d7054afd5f9b80272f34c3b
Adedoyin <isradeoluwa@gmail.com>
2021-01-22 15:49:37 +0100

    Splash Screen: Logo alpha animation implemented

--- app/src/main/java/com/example/twitterunfollowapp/SplashScreen.kt
+++ app/src/main/java/com/example/twitterunfollowapp/SplashScreen.kt
@@ -2,10 +2,14 @@ package com.example.twitterunfollowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+app_logo_splash_screen.animate().alpha(1.0F).setDuration(5000)
+
     }
 }
